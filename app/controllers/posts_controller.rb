9b641c22f6cd4cbecc91639ce996a33bd9afecb5
Kazuki0626 <kanchan210626@gmail.com>
2022-01-18 10:48:13 +0900

    create-post-delete 3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,9 @@ class PostsController < ApplicationController
     @post = Post.find_by(id: params[:id])
   end
   def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    @post.save
     redirect_to("/posts/index")
   end
 end
