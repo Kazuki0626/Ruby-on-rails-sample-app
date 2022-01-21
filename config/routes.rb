7fa3e7bded05fe66e786279c9360e5ab649cb9d8
Kazuki0626 <kanchan210626@gmail.com>
2022-01-21 23:43:44 +0900

    create-user-add-function-2 end

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  post "users/:id/update" => "users#update"
+  
+  get "users/:id/edit" => "users#edit"
+  post "users/create" => "users#create"
+  get "signup" => "users#new"
   get "users/index" => "users#index"
   get "users/:id" => "users#show"
-  get "signup" => "users#new"
-  post "users/create" => "users#create" 
 
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
